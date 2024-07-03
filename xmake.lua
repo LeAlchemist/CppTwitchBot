16bf4823bb89a10c1575bb1494473f82fe45701e
Lilian Gooch <omg.lily2000@gmail.com>
2024-07-03 15:43:45 -0500

    Improve and document support for Clang.

--- xmake.lua
+++ xmake.lua
@@ -7,29 +7,34 @@ set_languages("gnuxxlatest")
 -- TODO: Put these options in a Twitch group.
 
 option("oauth")
-    set_showmenu(true)
+set_showmenu(true)
 
 option("twitch_channel")
-    set_showmenu(true)
+set_showmenu(true)
 
 option("bot_username")
-    set_showmenu(true)
+set_showmenu(true)
 
+-- Build the Twitch bot.
 target("exe")
-    set_kind("binary")
-    set_warnings("all", "extra")
-    add_packages("boost")
-    add_packages("fmt")
-    add_options("oauth")
-    add_options("twitch_channel")
-    add_options("bot_username")
 
-    add_defines("OAUTH=\"" .. tostring(get_config("oauth")).."\"")
-    add_defines("CHANNEL=\"" .. tostring(get_config("twitch_channel")).."\"")
-    add_defines("BOTNAME=\"" .. tostring(get_config("bot_username")).."\"")
 
-    if is_mode("release") then
-        add_cxxflags("-fmerge-constants", {tools = "clang"})
-    end
+set_kind("binary")
+set_warnings("all", "extra")
+add_packages("boost")
+add_packages("fmt")
+add_options("oauth")
+add_options("twitch_channel")
+add_options("bot_username")
 
-    add_files("src/*.cpp")
+add_defines('OAUTH="' .. tostring(get_config("oauth")) .. '"')
+add_defines('CHANNEL="' .. tostring(get_config("twitch_channel")) .. '"')
+add_defines('BOTNAME="' .. tostring(get_config("bot_username")) .. '"')
+
+if is_mode("release") then
+    add_cxxflags("-fmerge-all-constants", { tools = "clang" })
+    add_cxxflags("-fexperimental-library", { tools = "clang", force = true })
+    add_cxxflags("-fmerge-constants", { tools = "gcc" })
+end
+
+add_files("src/*.cpp")
