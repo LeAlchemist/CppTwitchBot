a98103a9e7b8223667bcf4472394efec619e1ffb
Nicholas Ross <RossNicholasV@gmail.com>
2024-07-30 22:38:24 +0000

    Fix Compiler runtime

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,10 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate")
 add_requires("boost")
-add_requires("fmt")
+add_requires("fmt", {
+	configs = { header_only = true, toolchain = "llvm", runtimes = "c++_shared" },
+	system = false,
+})
 set_languages("gnuxxlatest")
 
 -- TODO: Enforce these being set.
@@ -17,37 +20,45 @@ option("bot_username")
 set_showmenu(true)
 
 option("DECL_BADGE_FEATURE")
-set_showmenu(true)
-set_default(true)
-add_defines("DECL_BADGE_FEATURE")
+if true then
+	set_showmenu(true)
+	set_default(true)
+	add_defines("DECL_BADGE_FEATURE")
+end
 
 option("DONT_SHOW_OAUTH")
-set_showmenu(true)
-set_default(true)
-add_defines("DONT_SHOW_OAUTH")
+if true then
+	set_showmenu(true)
+	set_default(true)
+	add_defines("DONT_SHOW_OAUTH")
+end
 
 -- Build the Twitch bot.
 target("exe")
+if true then
+	set_kind("binary")
+	set_warnings("allextra")
+	set_toolchains("llvm")
+	set_runtimes("c++_shared")
+	-- Experimental features are required for `jthread`.
+	add_cxxflags("-fexperimental-library", { force = true })
 
+	add_packages("boost")
+	add_packages("fmt")
+	add_options("oauth")
+	add_options("twitch_channel")
+	add_options("bot_username")
+	add_options("DECL_BADGE_FEATURE")
+	add_options("DONT_SHOW_OAUTH")
 
-set_kind("binary")
-set_warnings("all", "extra")
-add_packages("boost")
-add_packages("fmt")
-add_options("oauth")
-add_options("twitch_channel")
-add_options("bot_username")
-add_options("DECL_BADGE_FEATURE")
-add_options("DONT_SHOW_OAUTH")
+	add_defines('OAUTH="' .. tostring(get_config("oauth")) .. '"')
+	add_defines('CHANNEL="' .. tostring(get_config("twitch_channel")) .. '"')
+	add_defines('BOTNAME="' .. tostring(get_config("bot_username")) .. '"')
 
-add_defines('OAUTH="' .. tostring(get_config("oauth")) .. '"')
-add_defines('CHANNEL="' .. tostring(get_config("twitch_channel")) .. '"')
-add_defines('BOTNAME="' .. tostring(get_config("bot_username")) .. '"')
+	if is_mode("release") then
+		add_cxxflags("-fmerge-all-constants", { tools = "clang" })
+		add_cxxflags("-fmerge-constants", { tools = "gcc" })
+	end
 
-if is_mode("release") then
-    add_cxxflags("-fmerge-all-constants", { tools = "clang" })
-    add_cxxflags("-fexperimental-library", { tools = "clang", force = true })
-    add_cxxflags("-fmerge-constants", { tools = "gcc" })
+	add_files("src/*.cpp")
 end
-
-add_files("src/*.cpp")
